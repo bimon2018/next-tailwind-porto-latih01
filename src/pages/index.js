62add92f229808af6562422cd597fe3ce3037228
Ofbimon <ofbimon@gmail.com>
2023-03-09 10:42:05 +0700

    hampir selesai, kurang setting mode gelap/terang

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,18 +13,22 @@ import web4 from '../../public/web4.png';
 import web5 from '../../public/web5.png';
 import web6 from '../../public/web6.png';
 
+import { useState } from "react";
+
 
 export default function Home() {
+
+  const [darkMode, setDarkMode] = useState(false);
   
   return (
-    <>
+    <div className={darkMode ? "dark" : ""}>
       <Head>
         <title>My Portofolio</title>
         <meta name="description" content="A Portofolio page by Nextjs and Tailwindcss" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-white px-10 ">
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">developedbyMe</h1>
@@ -32,6 +36,7 @@ export default function Home() {
               <li>
                 <BsFillMoonStarsFill 
                   className="cursor-pointer text-2xl"
+                  onClick={() => setDarkMode(!darkMode)}
                 />
               </li>
               <li>
@@ -43,9 +48,9 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">Paijo Sonosukerto</h2>
-            <h3 className="text-2xl py-2">Frontend Developer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, aliquid! Nobis voluptatum laudantium hic inventore tempore eos id, explicabo dignissimos.</p>
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Paijo Sono Sukerto</h2>
+            <h3 className="text-2xl py-2 md:text-3xl">Developer and designer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-md mx-auto">Freelancer providing services for programming and design content needs. Join me down below and let's get cracking!</p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
             <AiFillTwitterCircle />
@@ -53,7 +58,7 @@ export default function Home() {
             <AiFillYoutube />
           </div>
           <div 
-            className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden"
+            className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96"
           >
             {/* DEPRECATED PROPS nextjs */}
             {/* <Image src={deved} layout="fill" objectFit="cover" /> */}
@@ -66,7 +71,7 @@ export default function Home() {
             <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
+              developer, I&apos;ve done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
@@ -79,9 +84,10 @@ export default function Home() {
           </div>
 
           <div className="lg:flex gap-10">
-            <div className="relative text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              {/* <Image src={design} width={100} height={100} alt=""  /> */}
-              <Image src={design} fill sizes="(max-width: 100px) 100px" alt=""  />
+            <div className="text-center shadow-lg hover:shadow-2xl p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <div className="flex justify-center">
+                <Image src={design} width={100} height={100} alt=""  />
+              </div>
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
@@ -97,7 +103,9 @@ export default function Home() {
             </div>
 
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} alt=""/>
+              <div className="flex justify-center">
+                <Image src={code} width={100} height={100} alt=""/>
+              </div>
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Code your dream project
               </h3>
@@ -113,7 +121,9 @@ export default function Home() {
             </div>
 
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} alt="" />
+              <div className="flex justify-center">
+                <Image src={consulting} width={100} height={100} alt="" />
+              </div>
               <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
               <p className="py-2">
                 Are you interested in feedback for your current project? I can
@@ -132,9 +142,86 @@ export default function Home() {
         <section>
           <div>
             <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
           </div>
+
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+
+            <div className="basis-1/3 flex-1 ">
+              <Image
+                className="rounded-lg object-cover"
+                width={'100%'}
+                height={'100%'}
+                src={web1}
+                alt=""
+              />
+            </div>
+
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={'100%'}
+                height={'100%'}
+                src={web2}
+                alt=""
+              />
+            </div>
+
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={'100%'}
+                height={'100%'}
+                src={web3}
+                alt=""
+              />
+            </div>
+
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={'100%'}
+                height={'100%'}
+                src={web4}
+                alt=""
+              />
+            </div>
+
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={'100%'}
+                height={'100%'}
+                src={web5}
+                alt=""
+              />
+            </div>
+
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={'100%'}
+                height={'100%'}
+                src={web6}
+                alt=""
+              />
+            </div>
+
+          </div>
+
         </section>
       </main>
-    </>
+    </div>
   )
 }
